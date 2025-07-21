dc573d1b7285722c3dafef9642cb7bb72511aa77
Vidyasagar Akhumukhi <sagar112113@gmail.com.com>
2025-07-21 15:57:15 +0100

    connecting to Prisma db and trying to generate Homepage componenets

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const AboutPage = () => {
-  return (
-    <div>
-      AboutPage
-    </div>
-  )
-}
+  return <div>aboutPAge</div>;
+};
 
-export default AboutPage
+export default AboutPage;
--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { LuAlignCenter, LuAlignLeft, LuAlignRight } from "react-icons/lu";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { links } from "@/utils/links";
 
 const LinksDropdown = () => {
-  return <div>LinksDropDown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex gap-4 max-w-[100px]">
+          <LuAlignRight className="w-7 h-7" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="center" sideOffset={10}>
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className="capitalize w-full">
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default LinksDropdown;
