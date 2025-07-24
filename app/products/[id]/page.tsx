313484322db42473d6812ef3657ac21c0c83a5a9
Vidyasagar Akhumukhi <sagar112113@gmail.com.com>
2025-07-24 15:20:27 +0100

    correnting type error in [id] of product

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,7 +5,15 @@ import { formatCurrency } from "@/utils/format";
 import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
-async function SingleProductPage({ params }: { params: { id: string } }) {
+
+type Props = {
+  params: {
+    id: string;
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
+};
+
+async function SingleProductPage({ params }: Props) {
   const product = await fetchSingleProduct(params.id);
   const { name, image, company, description, price } = product;
   const euroAmount = formatCurrency(price);
