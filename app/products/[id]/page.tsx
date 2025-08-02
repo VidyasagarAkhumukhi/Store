5de91676eeb89215ed02e028f6ef300874cafc7c
Vidyasagar Akhumukhi <sagar112113@gmail.com.com>
2025-08-02 17:14:03 +0100

    ShareOptions

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,6 +5,7 @@ import { formatCurrency } from "@/utils/format";
 import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
+import ShareButton from "@/components/single-product/ShareButton";
 
 export default async function SingleProduct({
   params,
@@ -34,7 +35,10 @@ export default async function SingleProduct({
         <div>
           <div className="flex gap-x-8 items-center">
             <h1 className="capitalize text-3xl font-bold">{name}</h1>
-            <FavoriteToggleButton productId={id} />
+            <div className="flex items-center gap-x-2">
+              <FavoriteToggleButton productId={id} />
+              <ShareButton name={product.name} productId={id} />
+            </div>
           </div>
           <ProductRating productId={id} />
           <h4 className="text-xl mt-2">{company}</h4>
