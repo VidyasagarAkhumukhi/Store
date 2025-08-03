4ea5a0b287a59cbad6cb4a0c71b7198d59d62440
Vidyasagar Akhumukhi <sagar112113@gmail.com.com>
2025-08-03 16:08:13 +0100

    order and sales dashboard for admin

---
+++ app/orders/loading.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+
+import LoadingTable from "@/components/global/LoadingTable";
+
+function loading() {
+  return <LoadingTable />;
+}
+export default loading;
